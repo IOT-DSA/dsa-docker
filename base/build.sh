98033f57f4fdeb605d7b1cbf7182c2626581e4df
Kenneth Endfinger <k.endfinger@dglogik.com>
2016-05-01 22:47:36 -0400

    Reorganize base images.

---
+++ base/build.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/env bash
+set -e
+
+for name in $(grep -l "FROM " -r . | grep "Dockerfile" | xargs dirname | sed 's/\.\///')
+do
+  tag="${name}"
+
+  if [[ "${name}" == "arm"* ]] && [[ "$(uname -m)" == "x86_64" ]]
+  then
+    continue
+  fi
+
+  cd "${name}"
+
+  if [[ "${name}" == "ubuntu" ]]
+  then
+    tag=latest
+  fi
+
+  run_build=true
+  if [[ "${@}" == *"--no-build"* ]]
+  then
+    run_build=false
+  fi
+
+  if [[ "${@}" == *"--build-${name}"* ]]
+  then
+    run_build=true
+  fi
+
+  if [[ "${run_build}" == "true" ]]
+  then
+    echo "==== Build base image for ${name} ===="
+    docker build -t "iotdsa/base:${name}" .
+  fi
+
+  if [[ "${@}" == *"--push"* ]]
+  then
+    docker push "iotdsa/base:${name}"
+  fi
+
+  if [[ "${name}" != "${tag}" ]]
+  then
+    docker tag "iotdsa/base:${name}" "iotdsa/base:${tag}"
+    if [[ "${@}" == *"--push"* ]]
+    then
+      docker push "iotdsa/base:${tag}"
+    fi
+  fi
+  cd ..
+done
