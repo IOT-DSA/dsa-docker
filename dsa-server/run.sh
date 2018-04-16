ed6b432695f0f770a9410ab91d54c30e7e28b57f
Julian Villalva <j.villalva@dglogik.com>
2018-04-16 14:48:39 -0700

    Copy build number into a location DSA will see it

--- dsa-server/run.sh
+++ dsa-server/run.sh
@@ -2,5 +2,8 @@
 set -e
 
 cd /data
+if [ ! -f /data/BUILD_NUMBER ]; then
+  cp /app/BUILD_NUMBER /data/
+fi
 echo "${DIST_URL}" > .docker
 exec /usr/bin/dart /app/bin/server_watcher.dart --log-file=logs/dglux_server.log
